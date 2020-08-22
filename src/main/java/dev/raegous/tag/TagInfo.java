7e5df5fcaaa722835ff8fac0ca692447317ad7e8
Tristan McPherson <shredder8910@gmail.com>
2020-08-21 23:24:37 -0400

    Initial plugin code

---
+++ src/main/java/dev/raegous/tag/TagInfo.java
@@ -0,0 +1,53 @@
+package dev.raegous.tag;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class TagInfo implements Serializable {
+    private final String filePath = "UltimateTag.data";
+
+    private String activeTaggerUsername;
+
+    public String getActiveTaggerUsername() {
+        return activeTaggerUsername;
+    }
+
+    public void setActiveTaggerUsername(String activeTaggerUsername) {
+        this.activeTaggerUsername = activeTaggerUsername;
+        saveData();
+    }
+
+    public boolean saveData() {
+        try {
+            BukkitObjectOutputStream out = new BukkitObjectOutputStream(new GZIPOutputStream(new FileOutputStream(filePath)));
+            out.writeObject(this);
+            out.close();
+            return true;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public TagInfo loadData() {
+        try {
+            BukkitObjectInputStream in = new BukkitObjectInputStream(new GZIPInputStream(new FileInputStream(filePath)));
+            TagInfo data = (TagInfo) in.readObject();
+            in.close();
+            return data;
+        } catch (ClassNotFoundException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/dev/raegous/tag/UltimateTagPlugin.java
@@ -0,0 +1,122 @@
+package dev.raegous.tag;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class UltimateTagPlugin extends JavaPlugin {
+    private TagInfo tagInfo;
+
+    private void setupTagData() {
+        tagInfo = tagInfo.loadData();
+        if (tagInfo == null) {
+            tagInfo = new TagInfo();
+            tagInfo.saveData();
+        }
+    }
+
+    public void tagRandomPlayer() {
+        Optional<? extends Player> randomPlayer = getRandom(Bukkit.getServer().getOnlinePlayers());
+        randomPlayer.ifPresent(this::tag);
+    }
+
+    @Override
+    public void onEnable() {
+        setupTagData();
+
+        getLogger().info("UltimateTag is enabled!");
+        tagRandomPlayer();
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("UltimateTag is disabled!");
+    }
+
+    private void tag(Player player) {
+        tag(player, null);
+    }
+
+    private void tag(Player player, Player by) {
+        tagInfo.setActiveTaggerUsername(player.getName());
+        getLogger().info("Setting current tagger to " + player.getName());
+
+        if (by == null) {
+            say(player.getName() + " is now it!");
+        } else {
+            say(player.getName() + " has been tagged by " + by.getName());
+        }
+    }
+
+    private void say(String message) {
+        Bukkit.getServer().broadcastMessage("[UltimateTag]: " + message);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (Bukkit.getOnlinePlayers().size() > 1 && tagInfo.getActiveTaggerUsername().isEmpty()) {
+            tagRandomPlayer();
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        // pvp
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            //player is attacking someone
+            Player attacker = (Player)event.getDamager();
+            Player attacked = (Player)event.getEntity();
+
+            if (attacker.getName().equals(tagInfo.getActiveTaggerUsername())) {
+                tag(attacked, attacker);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (event.getPlayer().getName().equals(tagInfo.getActiveTaggerUsername())) {
+            tagRandomPlayer();
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("tag")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
+            if (sender instanceof Player) {
+                if (!sender.hasPermission("ultimatetag.tag")) {
+                    sender.sendMessage("You can't change who is it");
+                    return false;
+                }
+            }
+
+            Player target = (Bukkit.getServer().getPlayer(args[0]));
+            if (target == null) {
+                sender.sendMessage(args[0] + " is not online!");
+                return false;
+            }
+
+            getLogger().info(sender.getName() + " tagged " + target.getName());
+
+            tag(target);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static <E> Optional<E> getRandom(Collection<E> e) {
+        return e.stream()
+                .skip((int) (e.size() * Math.random()))
+                .findFirst();
+    }
+}
